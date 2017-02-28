2600f332a55c393669e5abe9790bad2f4023bd61
Robert Christensen <robertchristensen1@mail.weber.edu>
2017-02-28 17:07:02 +0000

    start

---
+++ monaco_hw6_task1.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  monaco_hw6_task1.c
+ *       	Usage:  ./monaco_hw6_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2017 05:06:10 PM
+ *       Compiler:  gcc -Wall -Werr
+ *         Author:  Robert Christensen (), robertchristensen1@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
